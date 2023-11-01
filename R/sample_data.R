86334b3d40fe251bf7ba72764264763affa3c51b
qnq <shiaqiang2@gmail.com>
2023-11-01 10:00:41 +0800

    Initial commit

---
+++ R/sample_data.R
@@ -0,0 +1,10 @@
+#' @title Sample Data for Analysis
+#'
+#' @description A simple \code{tibble} that is for simple testing and running
+#' of example
+#' @format A \code{tibble} with 2 column, which are:
+#' \describe{
+#' \item{x}{random normal data}
+#' \item{y}{out come that is normal based on \code{x}}
+#' }
+"sample_data"
